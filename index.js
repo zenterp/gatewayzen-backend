40f05ac9feca067841c1b67b5630379264159392
Steven Zeiler <zeiler.steven@gmail.com>
2014-05-14 09:34:37 -0700

    [FEATURE] Add gateway model, ec2 client.

--- index.js
+++ index.js
@@ -1,87 +1,23 @@
-var GatewayQueue = require(__dirname+'/lib/gateway_queue');
-var DatabaseClient = require(__dirname+'/lib/gateways_database_client');
-var AwsClient = require(__dirname+'/lib/aws_client');
+var forever = require('forever-monitor');
 
-var queue = new GatewayQueue();
-var gateways = new DatabaseClient();
-var aws = new AwsClient();
+var processNames = [
+  'create_instances',
+  'confirm_instances'
+];
 
-queue.work(function(job, loop){
+processNames.forEach(function(name) {
 
-  switch(job.action) {
-  case 'keypair':
-    gateways.find(job.gateway_id, function(err, gateway){
-      if (err) {  
-        setTimeout(loop, 1000);
-        return;
-      };
-      awsClient.createKeypair(function(err, keypairId){
-        gateway.keypairId = keypairId;
-        gateway.save().complete(loop);
-      });
-    });
-    break;
-  case 'ec2instance':
-    gateways.find(job.gateway_id, function(err, gateway){
-      if (err) {  
-        setTimeout(loop, 1000);
-        return;
-      };
-      awsClient.createEc2Instance(keypairId, function(err, ec2InstanceId){
-        gateway.ec2Instance = ec2InstanceId;
-        gateway.save().complete(loop);
-      });
-    });
-    break;
-  default:
-    console.log(job);
-    setTimeout(loop, 1000);
-  }
-
-});
-
-gateways.onNewGateway(function(gateway, fn) {
-
-  var message = {
-    gateway_id: gateway.id,
-    action: 'keypair'
-  };
-
-  queue.send(message, function(err, resp){
-
-    gateway.state = 'keypair';
-    gateway.save().complete(fn);
-
+  var child = new (forever.Monitor)(__dirname+'/processes/'+name+'.js', {
+    max: 100,
+    silent: false,
+    options: []
   });
 
-});
-
-gateways.onNewKeypair(function(gateway, fn){
-
-  var message = {
-    gateway_id: gateway.id,
-    action: 'attachEc2Instance'
-  };
-
-  queue.send(message, function(err, resp){
-
-    gateway.state = 'ec2instance';
-    gateway.save().complete(fn);
-
+  child.on('exit', function () {
+    console.log(name + ' process has exited after 100 restarts');
   });
-   
-});
 
-gateways.onNewEc2Instance(function(gateway, fn){
-
-  console.log('*************************'); 
-  console.log('NEW EC2 INSTANCE CREATED!'); 
-  console.log('-------------------------'); 
-  console.log(gateway);
-
-  gateway.state = 'built';
-  gateway.save().complete(fn);
-  
+  child.start();
 });
 
 
