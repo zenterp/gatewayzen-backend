a39f1fc8b88ee07c315f4a1aa6401922083fb636
Steven Zeiler <zeiler.steven@gmail.com>
2014-12-30 06:54:52 +0000

    [FEATURE] Extract ResqueQueuer

--- resque.js
+++ resque.js
@@ -1,24 +1,19 @@
 var NR = require("node-resque");
 var ResqueWorker = require(__dirname+'/lib/resque_worker');
+var ResqueQueuer = require(__dirname+'/lib/resque_queuer');
 var jobs 	       = require('require-all-to-camel')(__dirname+'/lib/jobs');
 
-var connectionDetails = {
-  host:      "127.0.0.1",
-  password:  "",
-  port:      6379,
-  database:  0,
-}
-
 var worker = new ResqueWorker({
-  queues: ['initializeEc2Instance']
+  queues: ['gatewayzen']
 });
 
 worker.start();
 
-console.log(jobs);
+var queuer = new ResqueQueuer();
 
-var queue = new NR.queue({connection: connectionDetails}, jobs, function(){
-	queue.enqueue('initializeEc2Instance', 'initializeEc2Instance', 1);
-	queue.enqueue('initializeEc2Instance', 'initializeEc2Instance', 2);
+queuer.start().then(function(queue) {
+	queue.enqueue('gatewayzen', 'initializeEc2Instance', [1]);
+	queue.enqueue('gatewayzen', 'initializeEc2Instance', [2]);
+	queue.enqueue('gatewayzen', 'initializeEc2Instance', [3]);
 });
 
