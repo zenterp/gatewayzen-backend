7b8d346e50e13336eb1a20e062e43e23ce9ed240
Steven Zeiler <zeiler.steven@gmail.com>
2014-12-30 05:15:41 +0000

    [FEATURE] Extract ResqueWorker

--- resque.js
+++ resque.js
@@ -1,8 +1,5 @@
 var NR = require("node-resque");
-
-///////////////////////////
-// SET UP THE CONNECTION //
-///////////////////////////
+var ResqueWorker = require(__dirname+'/../');
 
 var connectionDetails = {
   host:      "127.0.0.1",
@@ -11,45 +8,19 @@ var connectionDetails = {
   database:  0,
 }
 
-//////////////////////////////
-// DEFINE YOUR WORKER TASKS //
-//////////////////////////////
-
 var jobs = {
   "setColdWallet": require(__dirname+'/lib/jobs/set_cold_wallet.js'),
   "fundColdWallet": require(__dirname+'/lib/jobs/fund_cold_wallet.js')
 };
 
-////////////////////
-// START A WORKER //
-////////////////////
-
-var worker = new NR.worker({connection: connectionDetails, queues: ['gateway']}, jobs, function(){
-  worker.workerCleanup(); // optional: cleanup any previous improperly shutdown workers
-  worker.start();
-});
-
-///////////////////////
-// START A SCHEDULER //
-///////////////////////
-
 var scheduler = new NR.scheduler({connection: connectionDetails}, function(){
   scheduler.start();
 });
+var worker = new ResqueWorker({
+  queues: ['gateway']
+});
 
-/////////////////////////
-// REGESTER FOR EVENTS //
-/////////////////////////
-
-worker.on('start',           function(){ console.log("worker started"); })
-worker.on('end',             function(){ console.log("worker ended"); })
-worker.on('cleaning_worker', function(worker, pid){ console.log("cleaning old worker " + worker); })
-worker.on('poll',            function(queue){ console.log("worker polling " + queue); })
-worker.on('job',             function(queue, job){ console.log("working job " + queue + " " + JSON.stringify(job)); })
-worker.on('reEnqueue',       function(queue, job, plugin){ console.log("reEnqueue job (" + plugin + ") " + queue + " " + JSON.stringify(job)); })
-worker.on('success',         function(queue, job, result){ console.log("job success " + queue + " " + JSON.stringify(job) + " >> " + result); })
-worker.on('error',           function(queue, job, error){ console.log("job failed " + queue + " " + JSON.stringify(job) + " >> " + error); })
-worker.on('pause',           function(){ console.log("worker paused"); })
+worker.start();
 
 scheduler.on('start',             function(){ console.log("scheduler started"); })
 scheduler.on('end',               function(){ console.log("scheduler ended"); })
@@ -57,10 +28,6 @@ scheduler.on('error',             function(error){ console.log("scheduler error 
 scheduler.on('poll',              function(){ console.log("scheduler polling"); })
 scheduler.on('working_timestamp', function(timestamp){ console.log("scheduler working timestamp " + timestamp); })
 scheduler.on('transferred_job',    function(timestamp, job){ console.log("scheduler enquing job " + timestamp + " >> " + JSON.stringify(job)); })
-
-////////////////////////
-// CONNECT TO A QUEUE //
-////////////////////////
 
 var queue = new NR.queue({connection: connectionDetails}, jobs, function(){
 	queue.enqueue('gateway', 'setColdWallet', [2]);
