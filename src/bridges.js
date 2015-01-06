f1ff8db1a68cd6df91ff23edf2eec6cb1185ba70
Steven Zeiler <zeiler.steven@gmail.com>
2015-01-05 22:32:56 -0800

    [TASK] Clean up

--- src/bridges.js
+++ src/bridges.js
@@ -1,13 +1,8 @@
-var path               = require('path')
 var BridgesApplication = require('bridges-application')
-var fs                 = require('fs')
 
 var application = new BridgesApplication({
   directory : __dirname
 })
 
-application.supervisor.start().then(function() {
-
-  console.log('started bridges application supervisor')
-})
+application.supervisor.start()
 
--- src/processes/instance.js
+++ src/processes/instance.js
@@ -1,23 +1,10 @@
-var NR           = require("node-resque")
 var path         = require('path')
 var ResqueWorker = require(path.join(__dirname, '/../lib/resque_worker'))
-var ResqueQueuer = require(path.join(__dirname, '/../lib/resque_queuer'))
-var requireAll   = require('require-all-to-camel')
-var jobs 	       = requireAll(path.join(__dirname, '/../jobs'))
 
 module.exports = function() {
 
-  var worker = new ResqueWorker({
+  new ResqueWorker({
     queues: ['gatewayzen']
-  })
-
-  worker.start()
-
-  var queuer = new ResqueQueuer()
-
-  queuer.start().then(function(queue) {
-
-    // queue.enqueue('gatewayzen', 'initializeEc2Instance', [1])
-  })
+  }).start()
 }
 
